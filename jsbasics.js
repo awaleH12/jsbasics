bc770dc3b6c848d994997a488735e6bc3db8266f
Hassan Aw <awale822@gmail.com>
2024-01-18 10:09:54 +0300

    form input id changed to confirmation, js code modified

--- jsbasics.js
+++ jsbasics.js
@@ -1,20 +1,20 @@
 function evcnumber() {
   phone = document.getElementById('evc').value;
-  validation = '';
+  confirmation = '';
 
   if (phone.length == 9) {
       if (phone.charAt(0) == '6') {
           if (phone.charAt(1) == '1' || phone.charAt(1) == '8') {
-              validation = "Correct Phone Number";
+              confirmation = "Correct Phone Number";
           } else {
-              validation = "Wrong Phone Number";
+              confirmation = "Wrong Phone Number";
           }
       } else {
-          validation = "Wrong Phone Number";
+          confirmation = "Wrong Phone Number";
       }
   } else {
-      validation = "Wrong Phone Number";
+      confirmation = "Wrong Phone Number";
   }
 
-  document.getElementById('validation').value = validation;
+  document.getElementById('confirmation').value = confirmation;
 }
