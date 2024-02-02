6d5ae7823a6307f0247a68a7f5b454e218051faa
Hassan Aw <awale822@gmail.com>
2024-02-02 15:21:49 +0300

    toggle updated but not complete

--- jsbasics.js
+++ jsbasics.js
@@ -1,10 +1,28 @@
-// timer
+let icon = document.getElementById("icon");
+let style = document.getElementById("style");
 
-const TS = 1000;
-function setTime(){
-    document.getElementById("timer").innerHTML = TS;
-    console.log(TS)
+icon.onclick = function(){
+  style.href = ""
+  // style.href = "white.css"
+  
+
+  if(style.href = "darktheme.css"){
+    icon.src = "icons/sunn-01.png"
+  } else if (style.href = "white.css") {
+    
+    icon.src = "icons/moonn-01-01.png"
+  }
+  // style.src ="darktheme.css"
+  // document.body.classList.toggle("darkmode");
+  // if(document.body.classList.contains("darkmode")){
+  //   icon.src = "icons/sunn-01.png"
+  // } else {
+  //   icon.src = "icons/moonn-01-01.png"
+  // }
+ 
 }
-setTime()
+ 
+// style theme
+// let style = document.getElementById("style");
 
-// document.getElementById("red").src="image.svg";
+// from chatgpt
